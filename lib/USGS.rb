34fee241fcc5d98ca8f9ba20f44e485089bce473
Jake Klingler <thearrow3456@gmail.com>
2013-09-26 23:06:15 -0400

    tweak region creation

--- lib/USGS.rb
+++ lib/USGS.rb
@@ -25,28 +25,30 @@ module USGS
   def create_regions
     #region calculation currently uses places from all 30 days...
     #eventually need to only use places within the past X days
+    regions = []
     Place.each do |place|
       nearby_quakes = Place
         .geo_near(place['geometry']['coordinates']).spherical
-        .max_distance(ENV['REGION_RADIUS'].to_f)
+        .max_distance(ENV['REGION_RADIUS'] || 40233.6)
 
       total_mag = 0.0
       total_quakes = 0.0
       nearby_quakes.each do |q|
         total_mag += q['properties']['mag']
-        total_quakes += 1
+        total_quakes += 1.0
       end
-      avg_mag = total_mag/total_quakes
+      avg_mag = total_mag.to_f / total_quakes.to_f
 
-      Region.create({
+      regions << {
           id: place['id'],
           properties: {
               place: place['properties']['place'],
               time: place['properties']['time'],
               avg_mag: avg_mag,
               title: avg_mag.to_s + ' - ' + place['properties']['place']}
-      })
+      }
     end
+    Region.create(regions)
   end
 
 end
