25c2533e2fe3f13fd12d29158c8dc6f7903a4f59
Jake Klingler <thearrow3456@gmail.com>
2013-09-26 20:44:40 -0400

    'count' and 'days' queries

--- app/controllers/quakes_controller.rb
+++ app/controllers/quakes_controller.rb
@@ -5,7 +5,14 @@ class QuakesController < ApplicationController
     days = params[:days] || 10
     region = params[:region]
 
-    render json: 'quakes?' + "count=#{count}&" + "days=#{days}&" + "region=#{region}"
+    DateTime.now.strftime('%s')
+
+    x_days_ago = (DateTime.now - days.to_i.days).strftime('%Q')
+    @places = Place.where("this.properties.time >= #{x_days_ago}")
+      .order_by("properties.mag DESC")
+      .limit(count)
+
+    render json: MultiJson.dump(@places, :pretty => true)
   end
 
 end
